542e66c774bd14532e471ba9e19dfe279792f348
Riad REKAB <hr_rekab@esi.dz>
2023-05-10 16:55:40 +0200

    added new API that returns news in a list

--- newsAPI/api/views.py
+++ newsAPI/api/views.py
@@ -1,6 +1,13 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import HttpResponse
+from rest_framework.response import Response
+
+
+from eventregistry import *
+er = EventRegistry(apiKey = '62fd2d2a-c90f-4cc1-9b72-ad5f85739368')
 
 
 @api_view(['GET'])
@@ -10,3 +17,24 @@ def getRoutes(request):
         {'POST': 'api/users/token/refresh/'},
     ]
     return Response(routes)
+
+
+class getNews(APIView) : 
+
+    def get(self, request):
+        q = QueryArticlesIter(
+        keywords = QueryItems.OR(["Elon Musk","Messi"]),
+        keywordsLoc = "body",
+        ignoreKeywords = "SpaceX",
+        dateStart = '2023-01-01',
+        dateEnd='2023-04-30',
+        lang='fra',
+        # lang = 'eng',
+        )
+        listAr = []
+        listAr = [article for article in q.execQuery(er, sortBy="rel", returnInfo=ReturnInfo(articleInfo=ArticleInfoFlags(concepts=True, categories=True)), maxItems=50)]
+        json_response = json.dumps(listAr, indent=4)
+
+        return Response(json_response)
+
+ 
