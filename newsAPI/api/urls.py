632e81d03008941b0e8799d356c8e9f675ae6603
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-05-16 05:51:43 +0200

    add serializers, views

--- newsAPI/api/urls.py
+++ newsAPI/api/urls.py
@@ -1,17 +1,25 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView
 )
 
 urlpatterns = [
-    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', views.getRoutes, name="routes"),
     path('users/register/', views.registerUser, name='register'),
-    path('users/login/', views.loginUser, name='login'),
+    path('users/valid-token/', views.validate_token, name='validate-token'),
+    path('users/logout/blacklist/', TokenBlacklistView.as_view(), name='blacklist'),
+
+    path('users/<str:username>/', views.getProfile, name='profile'),
+    path('users/update/<str:username>/', views.updateProfile, name='update-profile'),
+
+    path('topics/', views.getTopics, name='topics'),
+    path('topics/<str:username>/', views.getProfileTopics, name='topic'),
+
     path('getNews', views.getNews.as_view())
 ]
